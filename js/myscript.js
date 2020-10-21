39d302c7bcb7f2cbc4b2d6ebbd1858ec2901891b
Anna <rosalenanna@gmail.com>
2020-10-21 16:22:30 +0200

    struttura iniziale

---
+++ js/myscript.js
@@ -0,0 +1,31 @@
+// Creiamo un finto biglietto del treno con:
+
+// Codice treno (numero casuale tra 90000 e 100000 escluso)
+// Numero carrozza
+// Prezzo calcolato
+// Categoria selezionata dall’utente
+// Aggiungiamo una piccola animazione al click su “Crea” e “Annulla”, se clicchiamo su annulla dobbiamo ripulire il form.
+
+var prezzoFinale;
+
+// Nome passeggero
+var nomeUtente = document.getElementById('nome');
+
+console.log (nome.value);
+
+// km da percorrere
+var kmUtente = document.getElementById('km');
+
+console.log (km.value);
+
+// età passeggero
+var etaUtente = document.getElementById('fascia-eta');
+
+console.log (fascia-eta.value);
+
+// bottone Genera
+genera.addEventListener('click'
+  function() {
+    
+  }
+);
